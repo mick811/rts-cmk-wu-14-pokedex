f311ef994377a606fce91be57de56475fb7d7aed
mick811 <mikkelelvers@outlook.com>
2025-08-29 10:59:21 +0200

    added pokemon api types and query

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { Pokemon } from "@/types/api";
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
+function RouteComponent() {
+  const { data, error } = useQuery<Pokemon>({
+    queryKey: ['pokemon', 1],
+    queryFn: () => fetch('https://pokeapi.co/api/v2/pokemon/1').then(res => res.json()) as Promise<Pokemon>
+  });
 
-function RouteComponent() {}
+  if (error) return <div>Error: {error.message}</div>
+  if (!data) return <div>Loading...</div>
+  
+  return <img src={data.sprites.front_default ?? ""} alt="" />
+}
---
+++ src/types/api.ts
@@ -0,0 +1,222 @@
+export interface Pokemon {
+    abilities: Ability[];
+    base_experience: number;
+    cries: {
+        latest: string;
+        legacy: string;
+    };
+    forms: Form[];
+    game_indices: GameIndex[];
+    height: number;
+    held_items: HeldItem[];
+    id: number;
+    is_default: boolean;
+    location_area_encounters: string;
+    moves: Move[];
+    name: string;
+    order: number;
+    past_abilities?: PastAbility[];
+    past_types?: PastType[];
+    species: Species;
+    sprites: Sprites;
+    stats: Stat[];
+    types: Type[];
+    weight: number;
+}
+
+export interface Ability {
+    ability: {
+        name: string;
+        url: string;
+    };
+    is_hidden: boolean;
+    slot: number;
+}
+
+export interface Form {
+    name: string;
+    url: string;
+}
+
+export interface GameIndex {
+    game_index: number;
+    version: {
+        name: string;
+        url: string;
+    };
+}
+
+export interface HeldItem {
+    item: {
+        name: string;
+        url: string;
+    };
+    version_details: VersionDetail[];
+}
+
+export interface VersionDetail {
+    rarity: number;
+    version: {
+        name: string;
+        url: string;
+    };
+}
+
+export interface Move {
+    move: {
+        name: string;
+        url: string;
+    };
+    version_group_details: VersionGroupDetail[];
+}
+
+export interface VersionGroupDetail {
+    level_learned_at: number;
+    move_learn_method: {
+        name: string;
+        url: string;
+    };
+    order: number | null;
+    version_group: {
+        name: string;
+        url: string;
+    };
+}
+
+export interface PastAbility {
+    ability: {
+        name: string;
+        url: string;
+    };
+    is_hidden: boolean;
+    slot: number;
+    generation: {
+        name: string;
+        url: string;
+    };
+}
+
+export interface PastType {
+    generation: {
+        name: string;
+        url: string;
+    };
+    types: Type[];
+}
+
+export interface Species {
+    name: string;
+    url: string;
+}
+
+export interface Sprites {
+    back_default: string | null;
+    back_female: string | null;
+    back_shiny: string | null;
+    back_shiny_female: string | null;
+    front_default: string | null;
+    front_female: string | null;
+    front_shiny: string | null;
+    front_shiny_female: string | null;
+    other: {
+        dream_world: {
+            front_default: string | null;
+            front_female: string | null;
+        };
+        home: {
+            front_default: string | null;
+            front_female: string | null;
+            front_shiny: string | null;
+            front_shiny_female: string | null;
+        };
+        "official-artwork": {
+            front_default: string | null;
+            front_shiny: string | null;
+        };
+        showdown: {
+            back_default: string | null;
+            back_female: string | null;
+            back_shiny: string | null;
+            back_shiny_female: string | null;
+            front_default: string | null;
+            front_female: string | null;
+            front_shiny: string | null;
+            front_shiny_female: string | null;
+        };
+    };
+    versions: SpriteVersions;
+}
+
+export interface Stat {
+    base_stat: number;
+    effort: number;
+    stat: {
+        name: string;
+        url: string;
+    };
+}
+
+export interface Type {
+    slot: number;
+    type: {
+        name: string;
+        url: string;
+    };
+}
+
+export interface SpriteVersions {
+    "generation-i": {
+        "red-blue": GenerationSprites;
+        yellow: GenerationSprites;
+    };
+    "generation-ii": {
+        crystal: GenerationSprites;
+        gold: GenerationSprites;
+        silver: GenerationSprites;
+    };
+    "generation-iii": {
+        emerald: GenerationSprites;
+        "firered-leafgreen": GenerationSprites;
+        "ruby-sapphire": GenerationSprites;
+    };
+    "generation-iv": {
+        "diamond-pearl": GenerationSprites;
+        "heartgold-soulsilver": GenerationSprites;
+        platinum: GenerationSprites;
+    };
+    "generation-v": {
+        "black-white": GenerationSpritesAnimated;
+    };
+    "generation-vi": {
+        "omegaruby-alphasapphire": GenerationSprites;
+        "x-y": GenerationSprites;
+    };
+    "generation-vii": {
+        icons: {
+            front_default: string | null;
+            front_female: string | null;
+        };
+        "ultra-sun-ultra-moon": GenerationSprites;
+    };
+    "generation-viii": {
+        icons: {
+            front_default: string | null;
+            front_female: string | null;
+        };
+    };
+}
+
+export interface GenerationSprites {
+    back_default?: string | null;
+    back_female?: string | null;
+    back_shiny?: string | null;
+    back_shiny_female?: string | null;
+    front_default: string | null;
+    front_female?: string | null;
+    front_shiny?: string | null;
+    front_shiny_female?: string | null;
+}
+
+export interface GenerationSpritesAnimated extends GenerationSprites {
+    animated: GenerationSprites;
+}
